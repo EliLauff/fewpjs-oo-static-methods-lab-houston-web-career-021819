031f60e8ecc014aa06154967372bbd2b93492344
EliLauff <eli.lauffenburger@gmail.com>
2019-04-09 18:18:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9- ']+/g, "");
+  }
+
+  static titleize(str) {
+    let titleStr = [];
+    let counter = 0;
+    str.split(" ").forEach(word => {
+      if (
+        counter === 0 ||
+        (word !== "the" &&
+          word !== "a" &&
+          word !== "an" &&
+          word !== "but" &&
+          word !== "of" &&
+          word !== "and" &&
+          word !== "for" &&
+          word !== "at" &&
+          word !== "by" &&
+          word !== "from")
+      ) {
+        titleStr.push(this.capitalize(word));
+        counter += 1;
+      } else {
+        titleStr.push(word);
+        counter += 1;
+      }
+    });
+    return titleStr.join(" ");
+  }
 }
